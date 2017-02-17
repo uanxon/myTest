2fb51353e7519062eb827202eaeadeb22eeb7ba1
uanxon <uanxon@163.com>
2017-02-17 23:06:08 +0800

    first commit

---
+++ myGitTest/src/com/myGit/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.myGit;
+
+public class HelloWorld {
+	
+	
+	public HelloWorld (){
+		System.out.println("hi,this my test");
+	}
+	
+	public void say (){
+		System.out.println("hi,this is my test");
+	}
+	
+	public static void main(String[] args) {
+		
+		new HelloWorld().say();
+		
+	}
+
+}
